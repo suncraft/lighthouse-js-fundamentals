23d29cc6535032ef5b92604c7a702379beaeaaff
Suncraft <cookies666@msn.com>
2020-11-15 03:48:54 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,5 @@
-// const raining = false;
-// const cold = false;
+const raining = false;
+const cold = false;
 const temperature = -4;
 
 if (temperature < 0) {
@@ -9,5 +9,19 @@ if (temperature < 0) {
 } else {
   console.log("Short sleeves are fine");
 }
-
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
