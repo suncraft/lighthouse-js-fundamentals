38957115d68d1a585af169b9249eb911a514240e
Suncraft <cookies666@msn.com>
2020-12-11 03:54:41 +0000

    adding the hello functions

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+sayHello("ssss");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
